1aab44d97b331807f7169479c759fe35e671982b
Brian Amadori <brian@decentraland.org>
2022-03-16 11:57:05 -0300

    tests

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/SceneMetricsCounter/MetricsScoreUtils.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/SceneMetricsCounter/MetricsScoreUtils.cs
@@ -16,14 +16,10 @@ namespace DCL
 
         public static long ComputeTextureScore(Texture2D texture)
         {
-            // https://forum.unity.com/threads/does-unity-always-use-double-memory-for-texture-in-runtime.198270/
-            const double ARBITRARY_UNITY_MEMORY_MULTIPLIER = 1;
-
             // The mipmap memory increase should be actually 1.33 according to many sources
             const double MIPMAP_FACTOR = 1.4f; 
             const double BYTES_PER_PIXEL = 4;
-            return (long) (texture.width * texture.height * BYTES_PER_PIXEL * MIPMAP_FACTOR *
-                           ARBITRARY_UNITY_MEMORY_MULTIPLIER);
+            return (long) (texture.width * texture.height * BYTES_PER_PIXEL * MIPMAP_FACTOR);
         }
     }
 }
--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/SceneMetricsCounter/SceneMetricsHUD.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/SceneMetricsCounter/SceneMetricsHUD.cs
@@ -19,21 +19,44 @@ public class SceneMetricsHUD : MonoBehaviour
     {
         var currentSceneId = Environment.i.world.state.currentSceneId;
 
-        Texture2D newTexture = new Texture2D(512, 512);
-
+        Texture2D newTexture = new Texture2D(1024, 1024);
+        newTexture.Apply(true, true);
+        
         Rendereable r = new Rendereable();
         r.textures.Add(newTexture);
+        r.ownerId = "Blah";
 
         DataStore.i.sceneWorldObjects.AddRendereable(currentSceneId, r);
+
+        forceUpdate = true;
     }
+
+    private void AllocNewMesh()
+    {
+        var currentSceneId = Environment.i.world.state.currentSceneId;
+
+        Mesh newMesh = new Mesh();
+
+        Rendereable r = new Rendereable();
+        r.meshes.Add(newMesh);
+        r.ownerId = "Blah";
+
+        DataStore.i.sceneWorldObjects.AddRendereable(currentSceneId, r);
+        
+        forceUpdate = true;
+    }
+    
+    bool forceUpdate = false;
 
     void Update()
     {
         if (Environment.i == null)
             return;
 
-        if (Time.frameCount % 60 != 0)
+        if (Time.frameCount % 60 != 0 && !forceUpdate)
             return;
+
+        forceUpdate = false;
 
         SceneMetricsModel model = new SceneMetricsModel();
 
@@ -43,14 +66,36 @@ public class SceneMetricsHUD : MonoBehaviour
             model += sceneData;
         }
 
-        text.text = "";
+        text.text = "Global Stats:\n\n";
+
         text.text += $"Total Memory: {ToMb(model.totalMemoryScore)} (Profiler: {ToMb(model.totalMemoryProfiler)})\n\n";
-        text.text += $"Textures: {ToMb(model.textureMemoryScore)} (Profiler: {ToMb(model.textureMemoryProfiler)}\n";
+        text.text += $"Textures (x{model.textures}): {ToMb(model.textureMemoryScore)} (Profiler: {ToMb(model.textureMemoryProfiler)}\n";
         text.text +=
             $"Animations: {ToMb(model.animationClipMemoryScore)} (Profiler: {ToMb(model.animationClipMemoryProfiler)})\n";
-        text.text += $"Meshes: {ToMb(model.meshMemoryScore)} (Profiler: {ToMb(model.meshMemoryProfiler)})\n";
+        text.text += $"Meshes (x{model.meshes}): {ToMb(model.meshMemoryScore)} (Profiler: {ToMb(model.meshMemoryProfiler)})\n";
         text.text +=
-            $"AudioClips: {ToMb(model.audioClipMemoryScore)} (Profiler: {ToMb(model.audioClipMemoryProfiler)})\n";
+            $"AudioClips: {ToMb(model.audioClipMemoryScore)} (Profiler: {ToMb(model.audioClipMemoryProfiler)})\n\n";
+
+        string currentSceneId = Environment.i.world.state.currentSceneId;
+
+        if (string.IsNullOrEmpty(currentSceneId))
+            return;
+
+        var scene = Environment.i.world.state.loadedScenes[currentSceneId];
+        model = scene.metricsCounter.currentCount;
+
+        text.text +=
+            $"Current Scene Stats ({scene.sceneData.basePosition}... parcel count: {scene.sceneData.parcels.Length}):\n\n";
+
+        text.text += $"Total Memory: {ToMb(model.totalMemoryScore)} (Profiler: {ToMb(model.totalMemoryProfiler)})\n\n";
+        text.text +=
+            $"Textures (x{model.textures}): {ToMb(model.textureMemoryScore)} (Profiler: {ToMb(model.textureMemoryProfiler)}\n";
+        text.text +=
+            $"Animations: {ToMb(model.animationClipMemoryScore)} (Profiler: {ToMb(model.animationClipMemoryProfiler)})\n";
+        text.text +=
+            $"Meshes (x{model.meshes}): {ToMb(model.meshMemoryScore)} (Profiler: {ToMb(model.meshMemoryProfiler)})\n";
+        text.text +=
+            $"AudioClips: {ToMb(model.audioClipMemoryScore)} (Profiler: {ToMb(model.audioClipMemoryProfiler)})\n\n";
     }
 
     string ToMb(long memory)
